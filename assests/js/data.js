f3c50922b58cd571ea97634a956022b278144f1d
“Kannu-Mandora” <mandorakannu@gmail.com>
2022-11-06 12:19:11 +0530

    Fix: Bug Of adding empty fields and remove empty fields

--- assests/js/data.js
+++ assests/js/data.js
@@ -2,17 +2,27 @@ function getInputData() {
   const inputData = document.getElementById("enterData").value;
   const createNode = document.getElementById("nodes");
   const spanTag = document.createElement("span");
-  spanTag.textContent = inputData;
-  createNode.append(spanTag);
+  if(inputData===""){
+    alert("Enter Some TODO List!");
+  }
+  else{
+    spanTag.textContent = inputData;
+    createNode.append(spanTag);
+  }
 }
 function removeInputData() {
   const inputData = document.getElementsByTagName("span");
-  const ask = confirm("You want to delete all the entered list");
-  if (ask) {
-    const parent = document.getElementById("nodes");
-    parent.innerText = "";
-  } else {
-    return;
+  if(inputData.length===0){
+    alert("Enter Data First!");
+  }
+  else{
+    const ask = confirm("You want to delete all the entered list");
+    if (ask) {
+      const parent = document.getElementById("nodes");
+      parent.innerText = "";
+    } else {
+      return;
+    }
   }
 }
 const addText = document.querySelector("button");
