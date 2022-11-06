f4cb6455ca3b4e199243ed6fecb6f01f0911e28a
“Kannu-Mandora” <mandorakannu@gmail.com>
2022-11-06 12:47:16 +0530

    Format Texted.

--- assests/js/data.js
+++ assests/js/data.js
@@ -2,20 +2,18 @@ function getInputData() {
   const inputData = document.getElementById("enterData").value;
   const createNode = document.getElementById("nodes");
   const spanTag = document.createElement("span");
-  if(inputData===""){
+  if (inputData === "") {
     alert("Enter Some TODO List!");
-  }
-  else{
+  } else {
     spanTag.textContent = inputData;
     createNode.append(spanTag);
   }
 }
 function removeInputData() {
   const inputData = document.getElementsByTagName("span");
-  if(inputData.length===0){
+  if (inputData.length === 0) {
     alert("Enter Data First!");
-  }
-  else{
+  } else {
     const ask = confirm("You want to delete all the entered list");
     if (ask) {
       const parent = document.getElementById("nodes");
